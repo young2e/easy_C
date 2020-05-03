48a358d4ea788eee4c26f4184ec42607d78785da
young2e <64512044+young2e@users.noreply.github.com>
2020-05-03 14:14:55 +0900

    %f_and%lf

--- lesson04_3.c
+++ lesson04_3.c
@@ -5,11 +5,14 @@ int main(void)
 	
 	double height, width;
 
-	printf("�ﰢ���� ���̸� �Է��ϼ���. \n");
+	printf("삼각형의 높이를 입력하세요. \n");
 	scanf("%lf", &height);
 
-	printf("�ﰢ���� �غ��� �Է��ϼ���. \n");
+	printf("삼각형의 밑변을 입력하세요. \n");
 	scanf("%lf", &width);
 
-	printf("�ﰢ���� ���̴� %f�Դϴ�. \n", height * width / 2);
+	printf("삼각형의 넓이는 %f입니다. \n", height * width / 2);
 }
+
+// double형 데이터를 출력할 때에는 서식문자 %f를 사용하지만,
+// dobule형 데이터를 입력 받을 때에는 서식문자 %lf를 사용한다.
